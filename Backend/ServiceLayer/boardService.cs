58f0a4ad1cd89750b386a2b4988d45185b5608ff
ol-ozn <olgaoz@post.bgu.ac.il>
2022-04-21 17:53:23 +0300

    add user and board service interfaces

---
+++ Backend/ServiceLayer/boardService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public interface boardService
+{
+    /// <summary>
+    /// This method creates a new board. 
+    /// </summary>
+    /// <param name="boardName">The name of the new board</param>
+    /// <returns>The string "{}", unless an error occurs</returns>
+    public json add(string boardName);
+
+    /// <summary>
+    /// This method removes an existing board. 
+    /// </summary>
+    /// <param name="boardName">The name of the board to remove</param>
+    /// <returns>The string "{}", unless an error occurs</returns>
+    public json remove(string boardName);
+
+    /// <summary>
+    /// This method changes the state of task. 
+    /// </summary>
+    /// <param name="boardName">The name of the board in which the task is in</param>
+    /// <param name="taskTitle">The title of the task of which to change state</param>
+    /// <returns>The string "{}", unless an error occurs</returns>
+    public json changeState(string boardName, string taskTitle);
+
+    /// <summary>
+    /// This method changes the attributes of a task. 
+    /// </summary>
+    /// <param name="boardName">The name of the board in which the task is in</param>
+    /// <param name="taskTitle">The title of the task to be changed</param>
+    /// <param name="text">The new text to be inserted</param>
+    /// <returns>The string "{}", unless an error occurs</returns>
+    public json editTask(string boardName, string taskTitle, string text);
+
+}
---
+++ Backend/ServiceLayer/userService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public interface userService
+{
+
+    /// <summary>
+    ///  This method logs in an existing user.
+    /// </summary>
+    /// <param name="email">The email address of the user to login</param>
+    /// <param name="password">The password of the user to login</param>
+    /// <returns>Response with user email, unless an error occurs</returns>
+    public json login(string email, string password);
+
+    /// <summary>
+    /// This method creates a new account. 
+    /// </summary>
+    /// <param name="email">The email of the new user</param>
+    /// <param name="password">The password of the new user</param>
+    /// <returns>The string "{}", unless an error occurs</returns>
+	public json createUser(string email, string password);
+
+    /// <summary>
+    /// This method logs out a logged in user. 
+    /// </summary>
+    /// <returns>The string "{}", unless an error occurs</returns>
+    public json logout();
+
+    /// <summary>
+    /// This method deletes the current account. 
+    /// </summary>
+    /// <returns>The string "{}", unless an error occurs</returns>
+    public json deleteAccount();
+
+}
