88731ab4fe4f6a46296c9bdc2e96cf39fca9af0b
ol-ozn <olgaoz@post.bgu.ac.il>
2022-04-24 22:14:36 +0300

    add tests

--- Backend/ServiceLayer/boardService.cs
+++ Backend/ServiceLayer/boardService.cs
@@ -7,14 +7,14 @@ public interface boardService
     /// </summary>
     /// <param name="boardName">The name of the new board</param>
     /// <returns>The string "{}", unless an error occurs</returns>
-    public json add(string boardName);
+    public String add(string boardName);
 
     /// <summary>
     /// This method removes an existing board. 
     /// </summary>
     /// <param name="boardName">The name of the board to remove</param>
     /// <returns>The string "{}", unless an error occurs</returns>
-    public json remove(string boardName);
+    public String remove(string boardName);
 
     /// <summary>
     /// This method changes the state of task. 
@@ -22,7 +22,7 @@ public interface boardService
     /// <param name="boardName">The name of the board in which the task is in</param>
     /// <param name="taskTitle">The title of the task of which to change state</param>
     /// <returns>The string "{}", unless an error occurs</returns>
-    public json changeState(string boardName, string taskTitle);
+    public String changeState(string boardName, string taskTitle);
 
     /// <summary>
     /// This method changes the attributes of a task. 
@@ -31,6 +31,6 @@ public interface boardService
     /// <param name="taskTitle">The title of the task to be changed</param>
     /// <param name="text">The new text to be inserted</param>
     /// <returns>The string "{}", unless an error occurs</returns>
-    public json editTask(string boardName, string taskTitle, string text);
+    public String editTask(string boardName, string taskTitle, string text);
 
 }
---
+++ BackendTests/taskTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntroSE.Kanban.Backend.ServiceLayer;
+
+namespace BackendTests
+{
+    public class taskTest
+    {
+        private readonly GradingService gs;
+        public taskTest(GradingService gs)
+        {
+            this.gs = gs;
+        }
+        public void runTests()
+        {
+            String res1 = gs.UpdateTaskDueDate("yonatan@gmail.com", "milestone1", 2, 3, new DateTime(2022,04,26));
+            if(res1.Equals("good"))
+            {
+                Console.WriteLine("due date has been changed successfully");
+            } else
+            {
+                Console.WriteLine(res1);
+            }
+
+            String res2 = gs.UpdateTaskTitle("yonatan@gmail.com", "milestone1", 2, 3, "ms1");
+            if (res2.Equals("good"))
+            {
+                Console.WriteLine("task title has been changed successfully");
+            }
+            else
+            {
+                Console.WriteLine(res2);
+            }
+
+            String res3 = gs.UpdateTaskDescription("yonatan@gmail.com", "milestone1", 2, 3, "1st milestone out of many many many many...........");
+            if (res3.Equals("good"))
+            {
+                Console.WriteLine("task description has been changed successfully");
+            }
+            else
+            {
+                Console.WriteLine(res3);
+            }
+        }
+    }
+}
