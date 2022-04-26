b71041669f4e6e6bfffb98e387ee5f271979c8c5
ol-ozn <olgaoz@post.bgu.ac.il>
2022-04-26 20:47:21 +0300

    update userTest and userService

---
+++ Backend/ServiceLayer/Response.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer
+{
+    public class Response
+    {
+        public String ErrorMessage { get; set; }
+        public Object ReturnValue { get; set; }
+        public Response() {}
+        public Response(String ErrorMessage, Object ReturnValue)
+        {
+            this.ErrorMessage = ErrorMessage;
+            this.ReturnValue = ReturnValue;
+        }
+    }
+}
--- Backend/ServiceLayer/userService.cs
+++ Backend/ServiceLayer/userService.cs
@@ -9,26 +9,26 @@ public interface userService
     /// <param name="email">The email address of the user to login</param>
     /// <param name="password">The password of the user to login</param>
     /// <returns>Response with user email, unless an error occurs</returns>
-    public String login(string email, string password);
+    public String login(String email, String password);
     
     /// <summary>
     /// This method creates a new account. 
     /// </summary>
     /// <param name="email">The email of the new user</param>
     /// <param name="password">The password of the new user</param>
-    /// <returns>The string "{}", unless an error occurs</returns>
-	public String createUser(string email, string password);
+    /// <returns> goodJson, unless an error occurs</returns>
+	public String createUser(String email, String password);
 
     /// <summary>
     /// This method logs out a logged in user. 
     /// </summary>
-    /// <returns>The string "{}", unless an error occurs</returns>
+    /// <returns>goodJson, unless an error occurs</returns>
     public String logout();
 
     /// <summary>
     /// This method deletes the current account. 
     /// </summary>
-    /// <returns>The string "{}", unless an error occurs</returns>
+    /// <returns>goodJson, unless an error occurs</returns>
     public String deleteAccount();
 
 }
