89b04f767e0355067b18d2be1d779ab1ef774bb5
ol-ozn <olgaoz@post.bgu.ac.il>
2022-05-02 19:55:30 +0300

    starting business layer

---
+++ Backend/BusinessLayer/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+    public class Board
+    {
+        //private String name { get; set; }
+        private String name;
+        private List<List<Task>> board;
+        private readonly int id;
+        public Board(String boardName,int id)
+        {
+            name = boardName;
+            board = new List<List<Task>>();
+            id = id;
+        }
+
+        public String getName() { return name; }
+    }
+}
---
+++ Backend/BusinessLayer/BoardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+    public class BoardController
+    {
+        private List<Board> boards;
+
+        BoardController()
+        {
+            List<Board> boards = new List<Board>();
+        }
+
+        public Board addBoard(String boardName)
+        {
+            Board toAdd;
+            foreach (Board board in boards)
+            {
+                if (board.Equals(boardName))
+                    throw new Exception("There already exists a board with this name!");
+                
+            }
+            toAdd = new Board(boardName, 0); //TODO: after implementing id-counting, change the '0'
+            boards.Add(toAdd);
+            return toAdd;
+        }
+    }
+}
